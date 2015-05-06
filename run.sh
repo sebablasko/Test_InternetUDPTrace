ec6694d7efd11b99a1e21c1227193330284e3d7f
Sebastian <sebablasko@gmail.com>
2015-05-06 15:45:59 -0300

    added some comments for make a tracer of the running process

--- run.sh
+++ run.sh
@@ -12,6 +12,25 @@ echo 0 > /sys/kernel/debug/tracing/tracing_on
 #echo function_graph > /sys/kernel/debug/tracing/current_tracer
 echo function > /sys/kernel/debug/tracing/current_tracer
 
+# for i in $(pgrep server); do ps -mo pid,tid,fname,user,psr,sgi_p,%cpu,c,class,sched,wchan -p $i;done
+
+# meanning of the cols
+# pid:	process ID number of the process.
+# tid:	alias lwp: lwp (light weight process, or thread) ID of the lwp being reported. (alias spid, tid).
+# fname:(COMMAND) first 8 bytes of the base name of the process's executable file. The output in this column may contain spaces.
+# user:	effective user name. This will be the textual user ID, if it can be obtained and the field width permits, or a decimal representation otherwise.
+# psr:	processor that process is currently assigned to.
+# sgi_p:processor that the process is currently executing on. Displays "*" if the process is not currently running or runnable.
+# %cpu:	cpu utilization of the process in "##.#" format. Currently, it is the CPU time used divided by the time the process has been running (cputime/realtime ratio), expressed as a percentage. It will not add up to 100% unless you are lucky. (alias pcpu).
+# class:scheduling class of the process. (alias policy, cls). Field's possible values are:
+		#    -	not reported
+		#    TS	SCHED_OTHER
+		#    FF	SCHED_FIFO
+		#    RR	SCHED_RR
+		#    ?	unknown value
+# sched:scheduling policy of the process. The policies sched_other, sched_fifo, and sched_rr are respectively displayed as 0, 1, and 2
+# wchan:name of the kernel function in which the process is sleeping, a "-" if the process is running, or a "*" if the process is multi-threaded and ps is not displaying threads.
+
 num_port=1820
 num_threads=1
 num_clients=4
